0791a31fe32c572b9a8162ff834ea10ca473b20d
aarrodper <aarrodper@gmail.com>
2019-12-18 09:08:07 +0000

    tab

--- ruby/learning/04-chnet.rb
+++ ruby/learning/04-chnet.rb
@@ -1,35 +1,35 @@
 #!/usr/bin/env ruby
 
 def menu
-while true
-  puts "\n****************************"
-  puts "  CHOOSE AN OPTION, PLEASE"
-  puts "****************************"
-  puts " ( S ) SHOW CURRENT"
-  puts " ( R ) RESET"
-  puts " ( 1 ) aula109.static"
-  puts " ( 2 ) casa.static"
-  puts " ( E ) EXIT\n\n"
-case gets.strip
-when "s", "S"
-  system('cd /etc/sysconfig/network/')
-  system("awk 'FNR>=4 && FNR<=4' ifcfg-eth0") 
-when "r" , "R"
-  reset
-when "1"
- puts "You chose aula109.static"
- class109_static
-when "2"
- puts "You chose casa.static"
- home_static
-when "e" , "E"
-  puts "Exit... Bye!"
-  break
-else
-  puts "ERROR, unknown option."
-  break
-end
-end
+  while true
+    puts "\n****************************"
+    puts "  CHOOSE AN OPTION, PLEASE"
+    puts "****************************"
+    puts " ( S ) SHOW CURRENT"
+    puts " ( R ) RESET"
+    puts " ( 1 ) aula109.static"
+    puts " ( 2 ) casa.static"
+    puts " ( E ) EXIT\n\n"
+  case gets.strip
+    when "s", "S"
+      system('cd /etc/sysconfig/network/')
+      system("awk 'FNR>=4 && FNR<=4' ifcfg-eth0")
+    when "r" , "R"
+      reset
+    when "1"
+     puts "You chose aula109.static"
+     class109_static
+    when "2"
+     puts "You chose casa.static"
+     home_static
+    when "e" , "E"
+      puts "Exit... Bye!"
+      break
+    else
+      puts "ERROR, unknown option."
+      break
+    end
+  end
 end
 
 def reset
@@ -47,9 +47,8 @@ end
 
 def home_static
   system('sudo cp /home/aaron/net_config/home_static /etc/sysconfig/network/ifcfg-eth0')
-  reset 
+  reset
   puts "Network configuration completed"
 end
 
 menu
-
